b74b34144bd6f7a7f35acd7d786ba0c50d08dd3d
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-13 15:57:43 -0800

    Create a bundle

--- src/index.js
+++ src/index.js
@@ -10,10 +10,19 @@
 // include js and lodash script tags in index.html
 // edit package.json from main:index to private:true
 
+// First section done, but they state the problem is that while index.js does require the script Lodash, it isn't explicitly communicated. It works, but only because the lodash script is called before index.js.
+// npm install --save lodash
+// Add import lodash statement at top of index.js
+// Remove lodash script tag in html and alter src/script.js to simply "main.js"
+// npx webpack
+
+
+import _ from 'lodash';
+
 function component() {
     const element = document.createElement(`div`);
 
-    // Lodash, currently included via a script, is required for tis to work:
+    // Lodash, now imported by this script
     element.innerHTML = _.join([`hello`, `webpack`], ` `);
 
     return element;
