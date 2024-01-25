56129ff6e0d2b1195ecb84258a067512f26ac09b
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-01-25 12:22:44 -0800

    Import a function to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import _ from 'lodash';
+import myName from './myName';
+// import anotherFunction from './myName';
 
 function component(){
     const element = document.createElement(`div`);
-
     element.innerHTML = _.join([`Hello`, `webpack`], ` `);
-
     return element;
 }
+
+function sayHello(name){
+    const newElement = document.createElement(`div`);
+    newElement.innerHTML = myName(name);
+document.body.appendChild(newElement);
+    console.log(myName(name));
+}
+sayHello(`Kevin`)
+
+// anotherFunction();
 
 document.body.appendChild(component());
---
+++ src/myName.js
@@ -0,0 +1,8 @@
+const myName = (name) => `Hi! My name is ${name}`;
+
+
+function anotherFunction(){
+    console.log(`fuckery`);
+}
+
+export default myName;
